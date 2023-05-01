6cad2c81947c23566bf9240caab96463730fc4e5
Janick Spielmann <janick.spielmann@messageme.ch>
2023-05-01 13:52:26 +0200

    added Gruppe

---
+++ src/main/java/ch/gibb/Gruppe.java
@@ -0,0 +1,26 @@
+package ch.gibb;
+
+import ch.gibb.formen.Figur;
+
+import java.util.ArrayList;
+
+public class Gruppe {
+    private ArrayList <Figur> figuren = new ArrayList<>();
+
+    public Gruppe(ArrayList<Figur> figuren) {
+        this.figuren = figuren;
+    }
+
+    public void hinzufugen(Figur figur){
+        figuren.add(figur);
+    }
+
+    public void entfarnen(Figur figur){
+        figuren.remove(figur);
+    }
+
+    public void entfarnen(int position){
+        figuren.remove(position);
+    }
+
+}
