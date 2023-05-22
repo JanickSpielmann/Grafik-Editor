bdd41272e5cef17f1d15a14e20cd30751fd9494c
Janick Spielmann <janick.spielmann@messageme.ch>
2023-05-22 10:56:57 +0200

    moved MouseAdapter into EditorPanel

--- src/main/java/ch/gibb/modelView/EditorPanel.java
+++ src/main/java/ch/gibb/modelView/EditorPanel.java
@@ -1,6 +1,7 @@
 package ch.gibb.modelView;
 
 import java.awt.Graphics;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 
@@ -8,20 +9,35 @@ import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 final class EditorPanel extends JPanel {
-  private EditorControl editorControl;
+    private EditorControl editorControl;
 
-  EditorPanel(EditorControl control) {
-    editorControl = control;
-    addMouseListener(new EditorMouseAdapter());
-  }
+    private final class EditorMouseAdapter extends MouseAdapter {
 
-  // Die paintComponent()-Methode wird automatisch aufgerufen, wenn irgendwer die repaint()-
-  // Methode beim EditorFrame oder beim EditorPanel aufruft.
-  @Override
-  protected void paintComponent(Graphics g) {
-    super.paintComponent(g);
-    editorControl.allesNeuZeichnen(g);
-  }
+        @Override
+        public void mousePressed(MouseEvent e) {
+            super.mousePressed(e);
+            System.out.println(e.getPoint());
+        }
+
+        @Override
+        public void mouseReleased(MouseEvent e) {
+            super.mouseReleased(e);
+            System.out.println(e.getPoint());
+        }
+    }
+
+    EditorPanel(EditorControl control) {
+        editorControl = control;
+        addMouseListener(new EditorMouseAdapter());
+    }
+
+    // Die paintComponent()-Methode wird automatisch aufgerufen, wenn irgendwer die repaint()-
+    // Methode beim EditorFrame oder beim EditorPanel aufruft.
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        editorControl.allesNeuZeichnen(g);
+    }
 }
 
 
